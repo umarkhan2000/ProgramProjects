d8aa26b2870cfc4ff7d45822ec90431487845fe0
Umar Khan <umarazakhan@gmail.com>
2024-10-13 16:20:40 -0400

    LangChain_personalAssistant

---
+++ PersonalGPT/mygpt.py
@@ -0,0 +1,56 @@
+import os 
+import sys
+import constants
+
+# Import necessary pacakages to access OpenAI/ LangChain
+import openai
+from langchain_community.chains import ConversationalRetrievalChain, RetrievalQA
+from langchain_community.chat_models import ChatOpenAI
+from langchain_community.document_loaders import DirectoryLoader,TextLoader
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.indexes import VectorstoreIndexCreator
+from langchain_community.indexes.vectorstore import VectorStoreIndexWrapper
+from langchain_community.llms import OpenAI
+from langchain_community.vectorstores import Chroma
+
+# Retrieve API KEY from Constants.py file
+os.environ["API_KEY"] = constants.API_KEY
+
+# Enable to save to disk & reuse the model (for repeated queries on the same data)
+PERSIST = False
+
+query = None
+
+# Extract query when running .py script
+if len(sys.argv) > 1:
+  query = sys.argv[1]
+
+if PERSIST and os.path.exists("persist"):
+  print("Reusing index...\n")
+  vectorstore = Chroma(persist_directory="persist", embedding_function=OpenAIEmbeddings())
+  index = VectorStoreIndexWrapper(vectorstore=vectorstore)
+else:
+  #loader = TextLoader("data/data.txt") # Use this line if you only need data.txt
+  loader = DirectoryLoader("data/")
+  if PERSIST:
+    index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory":"persist"}).from_loaders([loader])
+  else:
+    index = VectorstoreIndexCreator().from_loaders([loader])
+
+chain = ConversationalRetrievalChain.from_llm(
+  llm=ChatOpenAI(model="gpt-4.0"),
+  retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
+)
+
+chat_history = []
+while True:
+  if not query:
+    query = input("Prompt: ")
+  if query in ['quit', 'q', 'exit']:
+    sys.exit()
+  result = chain({"question": query, "chat_history": chat_history})
+  print(result['answer'])
+
+  chat_history.append((query, result['answer']))
+  query = None
+
